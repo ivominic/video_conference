2a6c491da7d850da80fa8b9760e4ab05c80ea174
Ivo <ivo.minic@gmail.com>
2022-06-17 09:35:43 +0200

    addUser and set connection

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,26 @@
-var MyApp = (function () {
+let AppProcess = (function () {
+  function setNewConnection(connId) {
+    let iceConfiguration = {
+      iceServers: [
+        {
+          urls: "stun:stun.l.google.com:19302",
+        },
+        {
+          urls: "stun:stun1.l.google.com:19302",
+        },
+      ],
+    };
+    let connection = new RTCPeerConnection(iceConfiguration);
+  }
+
+  return {
+    setNewConnection: async function (connId) {
+      await setNewConnection(connId);
+    },
+  };
+})();
+
+let MyApp = (function () {
   let socket = null;
   let userId = "";
   let meetingId = "";
@@ -24,7 +46,18 @@ var MyApp = (function () {
 
     socket.on("inform_others_about_me", (data) => {
       addUser(data.otherUserId, data.connId);
+      AppProcess.setNewConnection(connId);
     });
+  }
+
+  function addUser(otherUserId, connId) {
+    let newDiv = $("#otherTemplate").clone();
+    newDiv = newDiv.attr("id", connId).addClass("other");
+    newDiv.find("h2").text(otherUserId);
+    newDiv.find("video").attr("id", "v_" + connId);
+    newDiv.find("audio").attr("id", "a_" + connId);
+    newDiv.show();
+    $("#divUsers").append(newDiv);
   }
 
   return {
